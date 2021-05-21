53a1ff224e5fa219f0917bf544829d7807fed675
Ethan N <ejnephew@yahoo.com>
2021-05-21 08:51:55 -0400

    Date picker and file picker.

---
+++ src/JavaFX_Filepicker/MainController.java
@@ -0,0 +1,46 @@
+package application;
+
+import java.io.File;
+import java.util.List;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+
+public class MainController {
+	@FXML
+	private Button btn1;
+	@FXML
+	private Button btn2;
+	@FXML
+	private ListView listView;
+	
+	public void Button1Action(ActionEvent event) {
+		FileChooser fc = new FileChooser();
+		fc.setInitialDirectory(new File ("C:\\"));
+		fc.getExtensionFilters().addAll(new ExtensionFilter("PDF Files", "*.pdf"));
+		File selectedFile = fc.showOpenDialog(null);
+		
+		if(selectedFile != null) {
+			listView.getItems().add(selectedFile.getAbsolutePath());
+		} else {
+			System.out.println("File not found.");
+		}
+	}
+	public void Button2Action(ActionEvent event) {
+		FileChooser fc = new FileChooser();
+		fc.setInitialDirectory(new File ("C:\\"));
+		fc.getExtensionFilters().addAll(new ExtensionFilter("PDF Files", "*.pdf"));
+		List<File> selectedFiles = fc.showOpenMultipleDialog(null);
+		
+		if(selectedFiles != null) {
+			for(int i = 0; i < selectedFiles.size(); i++)
+			listView.getItems().add(selectedFiles.get(i).getAbsolutePath());
+		} else {
+			System.out.println("File not found.");
+		}
+	}
+}
