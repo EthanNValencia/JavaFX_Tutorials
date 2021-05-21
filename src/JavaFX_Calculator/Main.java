e30c9234a7645093372bad56dd53039a534b0a1c
Ethan N <ejnephew@yahoo.com>
2021-05-21 08:45:44 -0400

    Calculator and bi-directional binding.

---
+++ src/JavaFX_Calculator/Main.java
@@ -0,0 +1,30 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+// 1 hour and 30 minutes into the video.
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			//BorderPane root = new BorderPane();
+			Parent root = FXMLLoader.load(getClass().getResource("Main.FXML"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Calculator");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
